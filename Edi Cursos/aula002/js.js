9a89e06662b9ccc9cd24e1cf0e6f4e9fd8855678
Gabriel Goulart <gabrielgoulartbnu@gmail.com>
2025-01-29 14:37:30 -0300

    Update

---
+++ Edi Cursos/aula002/js.js
@@ -0,0 +1,3 @@
+var titulo = document.querySelector(".titulo") //Pega todo o conteúdo
+document.querySelector(".titulo").textContent //Pega apenas o texto
+document.querySelector(".titulo").textContent = "Sistema - Edi Cursos" //Pega e altera o texto
