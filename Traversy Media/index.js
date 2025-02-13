80fd2282d2e3fc103796bbebbac000a512f084b2
Gabriel Goulart <gabrielgoulartbnu@gmail.com>
2025-02-13 20:45:36 -0300

    up

--- Traversy Media/index.js
+++ Traversy Media/index.js
@@ -14,7 +14,6 @@ list.push('Number4') //Adiciona um valor
 list.pop() //remove um valor
 console.log(list)
 
-console.clear()
 
 const person = {
     name: 'Juan',
@@ -25,4 +24,44 @@ console.log(person)
 
 for (let cont = 1; cont <= 10; cont++) {
     console.log(cont)
+}
+
+
+
+let age = 9
+if (age >= 18) {
+    console.log('Adult')
+} else if (age >= 15) {
+    console.log('Teen')
+} else if (age >= 4) {
+    console.log('Kid')
+} else {
+    console.log('Baby')
+}
+
+
+
+
+let color = 'blue'
+let verify = color == 'blue' ? 'true' : 'false'
+console.log(verify) //true
+
+
+
+
+const x = 9
+const color2 = x > 10 ? 'red' :'blue'
+
+console.clear()
+
+switch(color) {
+    case 'red':
+        console.log('red')
+    break
+    case 'blue':
+        console.log('blue')
+    break
+    default: 
+    console.log('Not red or blue')
+    break
 }
