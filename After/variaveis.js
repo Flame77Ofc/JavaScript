699d181ed86b65596fb3d7913cd0ae301b3fd5e5
Gabriel Goulart <gabrielgoulartbnu@gmail.com>
2025-02-19 20:03:49 -0300

    up

---
+++ After/variaveis.js
@@ -0,0 +1,19 @@
+let nomeDaVariavel = 5;
+let fruta = "Maçã";
+    console.log(fruta, nomeDaVariavel)
+
+
+//tipos de dados
+
+let nome = "Gabriel";
+
+let ContaBancaria = 760.90
+
+//
+let frutas = ["Maçã", "Banana", "Laranja"]
+frutas[0]
+
+//
+
+let xicara = { cor: "branco", material: "porcelona", peso: 100};
+console.log(xicara.cor)
