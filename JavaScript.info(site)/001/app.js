b3bb1a6bf518e3bd9e8c0ba4152a49206e72fc33
Gabriel Goulart <gabrielgoulartbnu@gmail.com>
2025-02-21 15:34:32 -0300

    update

---
+++ JavaScript.info(site)/001/app.js
@@ -0,0 +1,11 @@
+//É possível declarar múltiplas variáveis em uma única linha.
+let user = 'John', age = 24, message = 'Olá';
+
+//Para melhor legibilidade, é recomendável utilizar apenas única linha por variável.7
+let user2 = 'John';
+let age2 = 25;
+let message2 = 'Hello';
+
+//Uma variável pode em um momento ser uma string e então armazenar um número:
+let password = 'SecurityPassword'
+password = 123456
