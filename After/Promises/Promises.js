e50a6b889efc2c67dcc0722e3db9609b11d3a1a4
Gabriel Goulart <gabrielgoulartbnu@gmail.com>
2025-03-01 14:43:38 -0300

    Acabou JavaScript!!!

--- After/Promises/Promises.js
+++
@@ -1,9 +0,0 @@
-const pessoa = new Promise((resolve, reject) => {
-    let soma = 1 + 2
-    if (soma == 3) {
-        resolve('Deu Certo')
-    } else {
-        reject('Deu Errado')
-    }
-})
-console.log(pessoa)
